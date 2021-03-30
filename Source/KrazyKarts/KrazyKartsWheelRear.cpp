8845d092eaf03c6cc5775c7f45b42d2a34e2e2fd
Jeremy <jeremypnz@gmail.com>
2021-03-30 14:02:25 +1300

    First commit

---
+++ Source/KrazyKarts/KrazyKartsWheelRear.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KrazyKartsWheelRear.h"
+
+UKrazyKartsWheelRear::UKrazyKartsWheelRear()
+{
+	ShapeRadius = 35.f;
+	ShapeWidth = 10.0f;
+	bAffectedByHandbrake = true;
+	SteerAngle = 0.f;
+}
